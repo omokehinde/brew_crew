c2cd11fe98b6e22190447f388cb66e68df9fde42
omokehinde <engr.omokehinde@gmail.com>
2020-03-22 09:58:48 +0100

    initial commit

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,62 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  Register({Key key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  // text string state 
+  String email = '';
+  String password = '';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Colors.brown[100],
+       appBar: AppBar(
+         backgroundColor: Colors.brown[400],
+         elevation: 0,
+         title: Text("Signup to Brew Crew"),
+       ),
+       body: Container(
+         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+         child: Form(
+           child: Column(
+             children: <Widget>[
+               SizedBox(height: 20,),
+               TextFormField(
+                 onChanged: (val) {
+                   setState(() => email = val);
+                 },
+               ),
+               SizedBox(height: 20,),
+               TextFormField(
+                 obscureText: true,
+                 onChanged: (val) {
+                   setState(() => password = val);
+                 },
+               ),
+               SizedBox(height: 20,),
+               RaisedButton(
+                 color: Colors.pink[400],
+                 child: Text(
+                   "Register",
+                   style: TextStyle(color: Colors.white),
+                 ),
+                 onPressed: () async {
+                   print(email);
+                   print(password);
+                 },
+               ),
+             ],
+           ),
+         ),
+       ),
+    );
+  }
+}
